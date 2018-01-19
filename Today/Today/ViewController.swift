fc1b0ae1172a11d0207612a7ed4c3987d536f4b5
dan1002 <cscoi020@cscoi020ui-iMac.local>
2018-01-19 16:53:49 +0900

    추가

---
+++ Today/Today/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by cscoi020 on 2018. 1. 19..
+//  Copyright © 2018년 cscoi020. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+ 
+    
+    @IBAction func hi(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Today/Today/actor.swift
@@ -0,0 +1,8 @@
+
+import Foundation
+
+struct Actor{
+    var name: String
+    var team: String
+    var image: String
+}
